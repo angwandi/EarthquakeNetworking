cc72b89392b3d821cffe8982c05f9e737a52a844
Angwandi <demsdema217@gmail.com>
2018-04-30 13:07:30 +0100

    Split Location into two TextViews

--- app/src/main/java/com/example/demad/earthquakenetworking/EarthquakeAdapter.java
+++ app/src/main/java/com/example/demad/earthquakenetworking/EarthquakeAdapter.java
@@ -12,9 +12,7 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import java.util.List;
-
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     /**
      * Constructs a new {@link EarthquakeAdapter}.
@@ -22,33 +20,77 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
      * @param context     of the App
      * @param earthquakes is the list of earthquakes, which is the data source of the adapter
      */
-
     public EarthquakeAdapter(Context context, List<Earthquake> earthquakes) {
         super(context, 0, earthquakes);
     }
 
     /**
+     * The part of the location string from the USGS service that we use to determine
+     * <p>
+     * whether or not there is a location offset present ("5km N of Cairo, Egypt").
+     */
+    private static final String LOCATION_SEPARATOR = " of ";
+
+    /**
      * Returns a list of new item view that displays information about the earthquake
+     * <p>
      * in the the list of earthquakes.
      */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         @SuppressLint("ViewHolder") View listItemView = LayoutInflater.from(getContext()).inflate(R.layout.earthquake_list_item, parent, false);
+
         /*Find the earthquake at a given position*/
         Earthquake currentEarthquake = getItem(position);
-
         //        Find the TextView with the ID magnitude.
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magnitude);
         magnitudeTextView.setText(currentEarthquake.getMagnitude());
-
         //        Find the TextView with the ID location.
-        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location_offset);
-        locationTextView.setText(currentEarthquake.getLocation());
         // Create a new Date object from the time in milliseconds of the earthquake
         Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
         // Find the TextView with view ID date
         TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        /**
+         * String location split(String string)
+         * */
+        // Get the original location string from the Earthquake object,
+        // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
+        String originalLocation = currentEarthquake.getLocation();
+        // If the original location string (i.e. "5km N of Cairo, Egypt") contains
+        // a primary location (Cairo, Egypt) and a location offset (5km N of that city)
+        // then store the primary location separately from the location offset in 2 Strings,
+        // so they can be displayed in 2 TextViews.
+        String primaryLocation;
+        String locationOffset;
+        // Check whether the originalLocation string contains the " of " text
+        if (originalLocation.contains(LOCATION_SEPARATOR)) {
+            // Split the string into different parts (as an array of Strings)
+            // based on the " of " text. We expect an array of 2 Strings, where
+            // the first String will be "5km N" and the second String will be "Cairo, Egypt".
+            String[] parts = originalLocation.split(LOCATION_SEPARATOR);
+            // Location offset should be "5km N " + " of " --> "5km N of"
+            locationOffset = parts[0] + LOCATION_SEPARATOR;
+            // Primary location should be "Cairo, Egypt"
+            primaryLocation = parts[1];
+        } else {
+            // Otherwise, there is no " of " text in the originalLocation string.
+            // Hence, set the default location offset to say "Near the".
+            locationOffset = getContext().getString(R.string.near_the);
+            // The primary location will be the full location string "Pacific-Antarctic Ridge".
+            primaryLocation = originalLocation;
+        }
+        // Find the TextView with view ID location
+        TextView primaryLocationView = (TextView) listItemView.findViewById(R.id.primary_location);
+        // Display the location of the current earthquake in that TextView
+        primaryLocationView.setText(primaryLocation);
+        // Find the TextView with view ID location offset
+        TextView locationOffsetView = (TextView) listItemView.findViewById(R.id.location_offset);
+        // Display the location offset of the current earthquake in that TextView
+        locationOffsetView.setText(locationOffset);
+        /**
+         * String date using the below Helper Methods
+         * */
         // Format the date string (i.e. "Mar 3, 1984")
         String formattedDate = formatDate(dateObject);
         // Display the date of the current earthquake in that TextView
@@ -59,16 +101,12 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         String formattedTime = formatTime(dateObject);
         // Display the time of the current earthquake in that TextView
         timeView.setText(formattedTime);
-
-
         // Return the list item view that is now showing the appropriate data
         return listItemView;
     }
-
     /**
-     * ABOVE HELPER METHODS
+     * Date String  HELPER METHODS
      * */
-
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
      */
