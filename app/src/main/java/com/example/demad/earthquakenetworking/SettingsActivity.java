22bdd1f9fb4857646ef4d6735f4035d78f79a749
Angwandi <demsdema217@gmail.com>
2018-05-08 20:45:22 +0100

    Add Setting Activity

---
+++ app/src/main/java/com/example/demad/earthquakenetworking/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.example.demad.earthquakenetworking;
+
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+    public static class EarthquakePreferenceFragment extends PreferenceFragment{
+
+    }
+}
